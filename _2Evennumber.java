ddf2afb35fd7d654187da1530da0d5a8757ba74a
yadavpriya12 <169046727+yadavpriya12@users.noreply.github.com>
2024-08-06 14:11:43 +0530

    Add files via upload

---
+++ _2Evennumber.java
@@ -0,0 +1,13 @@
+public class _2Evennumber {
+    public static void main(String[] args) {
+        int i=100;
+        while(i>=1){
+   
+  if(i%2==0){
+    System.out.println(i);
+}
+
+i--;
+        }
+    }
+}
